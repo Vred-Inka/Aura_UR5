ee4a0085fd21a4f695a929c3768d3b1cf95a812c
Inna Shcherban <inna.shcherban@ubisoft.com>
2025-05-21 18:06:40 +0200

    Character Blueprint Setup

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -1,12 +1,13 @@
-// bla bla
-
+// Aura by Vredinka
 
 #include "Character/AuraCharacterBase.h"
-
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -1,4 +1,4 @@
-// bla bla
+// Aura by Vredinka
 
 #pragma once
 
@@ -16,4 +16,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 };
