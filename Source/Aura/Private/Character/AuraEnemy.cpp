2d29a1c15bbb587dae3cbdb01fb39f12db7d9fe6
Vredinka <ivsherban@gmail.com>
2025-07-15 18:19:14 +0200

    79. Attribute Menu- Text Value Button Row

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -42,9 +42,58 @@ void AAuraEnemy::BeginPlay()
 	InitAbilityActorInfo();
 }
 
+
+template<typename ReturnType, typename... Args>
+struct FunctionAdapterType
+{
+	using Type = ReturnType(*)(const FActiveGameplayEffectHandle&, Args&&...);
+};
+class AnyFakeFunctionPointer
+{
+};
+
+using FunctionPointer = void*;
+AnyFakeFunctionPointer m_Callback;
+FunctionPointer m_CallbackAdapter{ nullptr };
+
+template<typename... Args>
+void Foo(Args&&... args)
+{
+	std::vector<FActiveGameplayEffectHandle> handlers;
+	for (const FActiveGameplayEffectHandle& handler : handlers)
+	{
+		using AdapterFunctionType = typename FunctionAdapterType<int, Args...>::Type;
+		AdapterFunctionType callbackAsFuncType = reinterpret_cast<AdapterFunctionType>(m_CallbackAdapter);
+
+		const int result = callbackAsFuncType(handler, std::forward<Args>(args)...);
+	}
+}
+
+
+class Body
+{
+public:
+	float GetFilterInfo(){ return filterInfo; }
+	void SetFilterInfo(float info){ filterInfo  =  info;}
+	
+private:
+	float filterInfo {2.0f};
+};
+
+void Foo1(Body& body)
+{
+	const auto filterInfo = body.GetFilterInfo();
+
+	for (int i = 0; i < 10; ++i)
+	{
+		Body b;
+		b.SetFilterInfo(filterInfo);
+	}
+	
+}
+
 void AAuraEnemy::InitAbilityActorInfo()
 {	
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
-	
 }
