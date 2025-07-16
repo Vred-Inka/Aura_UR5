c3f8fc1d5d3c7d7dc4eb293e14ad5971f01e8e99
Vredinka <ivsherban@gmail.com>
2025-07-16 18:44:31 +0200

    90 Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Aura by Vredinka
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitualValues()
+{
+	
+}
