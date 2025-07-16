dd95f935143e8c93112f093b485f914fb18b0231
Vredinka <ivsherban@gmail.com>
2025-07-16 16:20:12 +0200

    86 Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Aura by Vredinka
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	 UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attribute.Secondary.Armor")
+	 	, FString("Reduces damage taken, improves Block Chance."));
+}
