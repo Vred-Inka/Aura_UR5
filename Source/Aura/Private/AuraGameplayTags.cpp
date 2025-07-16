1f893d1d56c9271e1882d09df89ca8527a35522e
Vredinka <ivsherban@gmail.com>
2025-07-16 16:45:47 +0200

    87 Aura Asset Manager

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -8,6 +8,7 @@ FAuraGameplayTags FAuraGameplayTags::GameplayTags;
 
 void FAuraGameplayTags::InitializeNativeGameplayTags()
 {
-	 UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attribute.Secondary.Armor")
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get()
+		.AddNativeGameplayTag(FName("Attributes.Secondary.Armor")
 	 	, FString("Reduces damage taken, improves Block Chance."));
 }
