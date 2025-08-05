08bc7840ccf684cb7b20adf8378be24cc449a21a
Vredinka <ivsherban@gmail.com>
2025-08-05 15:32:09 +0200

    134. Set By Caller Attribute

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -101,5 +101,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("InputTag.4"),
 			FString("Input Tag for 4 key."));
+
+	GameplayTags.Damage = UGameplayTagsManager::Get()
+	.AddNativeGameplayTag(FName("Damage"),
+		FString("Input Tag for Damage"));
 	
 }
