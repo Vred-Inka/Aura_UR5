eee3d6a9308de1df021bdd38e80970bff287eb0c
Vredinka <ivsherban@gmail.com>
2025-08-27 18:37:18 +0200

    187. Ghoul Enemy

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -44,13 +44,13 @@ public:
 	
 	void HitReactTagChanged(const FGameplayTag CallbackTag, int32 NewCount);
 
-	UPROPERTY(BlueprintReadOnly, Category= "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Combat")
 	bool bHitReacting = false;
 
-	UPROPERTY(BlueprintReadOnly, Category= "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Combat")
 	float BaseWalkSpeed = 250.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Combat")
+	UPROPERTY(BlueprintReadOnly, Category= "Combat")
 	float LifeSpan = 5.f;
 
 	UPROPERTY(BlueprintReadWrite, Category= "Combat")
