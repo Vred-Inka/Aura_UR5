eed646a62b78d674f49848e998c8a427f0931c28
Vredinka <ivsherban@gmail.com>
2025-08-29 16:59:29 +0200

    195. Playing the Ranged Attack Montage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "Interaction/CombatInterface.h"
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
@@ -17,4 +18,15 @@ void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(),
 		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
+}
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontage(const TArray<FTaggedMontage>& TaggedMontages) const
+{
+	if (TaggedMontages.Num() > 0)
+	{
+		const int32 SelectItem = FMath::RandRange(0, TaggedMontages.Num() - 1);
+		return TaggedMontages[SelectItem];
+	}
+
+	return FTaggedMontage();
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -24,4 +24,7 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
 	TMap<FGameplayTag, FScalableFloat> DamageTypes;
+
+	UFUNCTION(BlueprintPure)
+	FTaggedMontage GetRandomTaggedMontage(const TArray<FTaggedMontage>& TaggedMontages) const;
 };
