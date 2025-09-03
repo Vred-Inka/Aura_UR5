2f766df4487d258f8801f081ef66fffb8e2c9e82
Vredinka <ivsherban@gmail.com>
2025-09-03 17:53:21 +0200

    208. Goblin Spear -  Hurt and Death Sounds

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -95,8 +95,11 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	UNiagaraSystem* BloodEffect;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	USoundBase* DeathSound;
 	
 private:	
 	UPROPERTY(EditAnywhere, Category="Abilities")
