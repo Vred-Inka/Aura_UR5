3e742c19abadb0f1db0d331672a743e33ec1c425
Vredinka <ivsherban@gmail.com>
2025-11-27 02:40:59 +0100

    301. Globe Reassigned

--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -9,7 +9,8 @@
 #include "SpellMenuWidgetController.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquipButtonEnabled, FString, DescriptionString, FString, NextLevelDescriptionString);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitForEquipSelection, const FGameplayTag&, AbilityType);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitForEquipSelectionSignature, const FGameplayTag&, AbilityType);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSpellGlobeReassignedSignature, const FGameplayTag&, AbilityTag);
 
 struct FSelectedAbility
 {
@@ -36,10 +37,13 @@ public:
 	FSpellGlobeSelectedSignature SpellGlobeSelectedDelegate;
 	
 	UPROPERTY(BlueprintAssignable)
-	FWaitForEquipSelection WaitForEquipDelegate;
+	FWaitForEquipSelectionSignature WaitForEquipDelegate;
 
 	UPROPERTY(BlueprintAssignable)
-	FWaitForEquipSelection StopWaitingForEquipDelegate;
+	FWaitForEquipSelectionSignature StopWaitingForEquipDelegate;
+
+	UPROPERTY(BlueprintAssignable)
+	FSpellGlobeReassignedSignature SpellGlobeReassignedDelegate;
 
 	UFUNCTION(BlueprintCallable)
 	void SpellGlobeSelected(const FGameplayTag& AbilityTag);
