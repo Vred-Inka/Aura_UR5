67a6c6f195f65231f52987132e78b83d54d14051
Vredinka <ivsherban@gmail.com>
2026-01-04 15:50:38 +0100

    345. Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,29 @@
+// Aura by Vredinka
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{ 
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+}
+
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,25 @@
+// Aura by Vredinka
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMagicCircle();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+};
